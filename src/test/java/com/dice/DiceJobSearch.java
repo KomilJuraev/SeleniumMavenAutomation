6921d62767cc8b8b09b0c4321fd96bb5cced97c1
KomilJuraev <k.juraev1994@gmail.com>
2018-06-13 20:22:07 -0400

    HOMEWORK added

--- src/test/java/com/dice/DiceJobSearch.java
+++ src/test/java/com/dice/DiceJobSearch.java
@@ -1,6 +1,11 @@
 package com.dice;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
 import java.time.LocalDateTime;
+import java.util.ArrayList;
 import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.By;
@@ -9,7 +14,7 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class DiceJobSearch {
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 		// Set up chrome driver path
 		WebDriverManager.chromedriver().setup();
 		;
@@ -36,31 +41,49 @@ public class DiceJobSearch {
 			throw new RuntimeException("Step Failed");
 		}
 
-		String keyword = "java developer";
-		driver.findElement(By.id("search-field-keyword")).clear();
-		driver.findElement(By.id("search-field-keyword")).sendKeys(keyword);
+		FileReader fr = new FileReader("KEYWORDS.txt");
+		BufferedReader br = new BufferedReader(fr);
+		ArrayList<String> keywords = new ArrayList<>();
+		for (int i = 0; i < 20; i++) {
+			keywords.add(br.readLine());
+		}
+		
+		ArrayList<String> content = new ArrayList<>();
+		String keyword;
+		for (int i = 0; i < keywords.size(); i++) {
+			keyword = keywords.get(i);
+			driver.findElement(By.id("search-field-keyword")).clear();
+			driver.findElement(By.id("search-field-keyword")).sendKeys(keyword);
 
-		String location = "21117";
-		driver.findElement(By.id("search-field-location")).clear();
-		driver.findElement(By.id("search-field-location")).sendKeys(location);
+			String location = "21117";
+			driver.findElement(By.id("search-field-location")).clear();
+			driver.findElement(By.id("search-field-location")).sendKeys(location);
 
-		driver.findElement(By.id("findTechJobs")).click();
+			driver.findElement(By.id("findTechJobs")).click();
 
-		String count = driver.findElement(By.id("posiCountId")).getText();
-		System.out.println(count);
+			String count = driver.findElement(By.id("posiCountId")).getText();
 
-		int countResult = Integer.parseInt(count.replaceAll(",", ""));
+			int countResult = Integer.parseInt(count.replaceAll(",", ""));
+			
+			content.add("Keyword : " + keyword + " | Number of Jobs Available " + countResult);
 
-		if (countResult > 0) {
-			System.out.println(
-					"Step Pass: Keyword " + keyword + " search returned " + countResult + " results int " + location);
-		} else {
-			System.out.println(
-					"Step failed: Keyword " + keyword + " search returned " + countResult + " results int " + location);
+			if (countResult > 0) {
+				System.out.println("Step Pass: Keyword " + keyword + " search returned " + countResult + " results int "
+						+ location);
+			} else {
+				System.out.println("Step failed: Keyword " + keyword + " search returned " + countResult
+						+ " results int " + location);
+			}
+
+			driver.navigate().back();
 		}
 
-		driver.close();
-		System.out.println("Test Completed - " + LocalDateTime.now());
+		 driver.close();
+		 System.out.println("Test Completed - " + LocalDateTime.now());
+		 
+		 for(int i = 0; i < content.size(); i++ ) {
+			 System.out.println(content);
+		 }
 	}
 
 }
